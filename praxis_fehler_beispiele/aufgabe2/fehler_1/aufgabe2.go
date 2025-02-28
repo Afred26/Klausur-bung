9e2f662d3cf45a94c436123a48454f0115efdb21
Afred26 <schall.fredrik@gmail.com>
2025-02-28 14:28:35 +0100

    Aufgabe 2  bearbeitet

--- praxis_fehler_beispiele/aufgabe2/fehler_1/aufgabe2.go
+++ praxis_fehler_beispiele/aufgabe2/fehler_1/aufgabe2.go
@@ -3,6 +3,12 @@ package aufgabe2
 // ExcludeBetween erwartet eine Liste und zwei Zahlen m und n.
 // Die Funktion liefert eine Liste mit allen Elementen x, für die gilt: m < x < n.
 func ExcludeBetween(list []int, m, n int) []int {
-	//Todo
-	return []int{}
+	result := []int{}
+	for _, el := range list {
+		if el > m && el < n {
+			result = append(result, el)
+		}
+	}
+
+	return result
 }
