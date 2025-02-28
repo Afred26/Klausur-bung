41d53e61882e6b0c01ff32bee27271c20e6487b8
Afred26 <schall.fredrik@gmail.com>
2025-02-28 15:35:24 +0100

    Aufgabe 6 mit Schleife gelöst

--- praxis_fehler_beispiele/aufgabe6/fehler_1/aufgabe6.go
+++ praxis_fehler_beispiele/aufgabe6/fehler_1/aufgabe6.go
@@ -6,17 +6,38 @@ package aufgabe6
 // Im Ergebnis kommt jedes Element nur einmal vor.
 // Die Elemente stehen im Ergebnis in der Reihenfolge ihres ersten Auftretens.
 func Duplicates(list []int) []int {
-	// Todo
-	return []int{}
+	result := []int{}
+	repeats := []int{}
+	remember := []int{}
+	for _, el := range list {
+		if !Contains(repeats, el) {
+			if Contains(remember, el) {
+				repeats = append(repeats, el)
+			}
+			if !Contains(remember, el) {
+				remember = append(remember, el)
+			}
+		}
+
+	}
+	for _, el := range list {
+		if !Contains(result, el) {
+			if Contains(repeats, el) {
+				result = append(result, el)
+			}
+		}
+	}
+	return result
 }
 
 // überpüfen ob el nochmal in liste ist fehlt mir
 // Hilfsfunktion Contains
-func Contains(l []int, el int) bool {
-	for _, e := range l {
-		if e == el {
-
+func Contains(l []int, x int) bool {
+	result := false
+	for _, el := range l {
+		if el == x {
+			result = true
 		}
 	}
-	return false
+	return result
 }
